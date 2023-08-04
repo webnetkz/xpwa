4d5e1fb45dbff05568b7668ee216a92deecef3ad
webnetkz <dbqqbq@gmail.com>
2023-08-04 14:28:18 +0600

    THIS IS LAST POINT

--- xpwa/DOM/components/progress/StepProcessing.js
+++ xpwa/DOM/components/progress/StepProcessing.js
@@ -42,7 +42,7 @@ export class StepProcessingXPWA extends HTMLElement
             }
         });
 
-        indicatorLine.style.width = `${stepWidth * currentStep}%`; // Перемещаем индикатор в соответствии с текущим шагом
+        indicatorLine.style.width = `${stepWidth * currentStep}%`;
     }
     
     static get observedAttributes()
