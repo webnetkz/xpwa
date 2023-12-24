8a58dedc12389e93fb58cd021d2fe8a264ef72cb
webnetkz <dbqqbq@gmail.com>
2023-12-25 03:08:55 +0600

    Publish 0.8.64, update devops

--- publish.sh
+++ publish.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-old_string="211-se"
-new_string="0.8.63"
+old_string="0.8.63"
+new_string="0.8.64"
 
 replace_string_in_file() {
     file=$1
     sed -i '' "s/$old_string/$new_string/g" $file
 }
 
-files=("./xpwa.js" "./package.json" "./readme.md")
+files=("./xpwa.js" "./package.json" "./readme.md" "./webpack.config.js")
 
 for file in "${files[@]}"; do
     if [ -e "$file" ]; then
@@ -19,13 +19,13 @@ for file in "${files[@]}"; do
     fi
 done
 
-npx webpack;
-
-sleep 5;
-
-# Раскомментировать сроку стилей
-
 file="./xpwa/DOM/DOM.js"
 line_number=1
+
+sed -i '' -e "${line_number}s/^\/\/\(.*\)/\1/" "$file"
+echo "Строка в файле $file с номером $line_number была раскомментирована."
+
+npx webpack;
+
 sed -i '' -e "${line_number}s/\(.*\)/\/\/\1/" "$file"
 echo "Строка в файле $file с номером $line_number была закомментирована."
