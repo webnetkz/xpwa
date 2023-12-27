a27ca3e79f82b57ababca551afbbf9667930f43d
webnetkz <dbqqbq@gmail.com>
2023-12-28 00:00:23 +0600

    Update options xpwa connect sw.js file

--- xpwa/options.js
+++ xpwa/options.js
@@ -96,7 +96,7 @@ class Options {
     pwa() {
         if('serviceWorker' in navigator) {
 			window.addEventListener('load', function() {
-				navigator.serviceWorker.register('/sw.js').then(function(registration) {
+				navigator.serviceWorker.register('./sw.js').then(function(registration) {
 					console.log('ServiceWorker registration successful with scope: ', registration.scope);
 				}, function(err) {
 					console.log('ServiceWorker registration failed: ', err);
