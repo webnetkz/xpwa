4d57fd639ed3bdf635d758796727d2a73233fa55
webnetkz <dbqqbq@gmail.com>
2023-12-29 02:21:34 +0600

    Исправлены ошибки, адаптирован контент под nav

--- xpwa/DOM/components/navigation/Nav.js
+++ xpwa/DOM/components/navigation/Nav.js
@@ -2,25 +2,38 @@ export class NavXPWA extends HTMLElement {
   constructor() {
     super();
 
+    let contentBlock = this.checkContentBlock();
+
     this.addEventListener('mousemove', () => {
       this.style.cssText = `
-      transition-duration: 400ms;
-      left: 0;
-      background: rgba(0, 0, 0, 0);
-      `;
+        transition-duration: 400ms;
+        left: 0;
+        background: rgba(0, 0, 0, 0);`;
+      if(contentBlock) {
+        contentBlock.style.cssText = `
+          left: 5vw;`;
+      }
     });
 
     document.body.addEventListener('click', () => {
+      if(contentBlock) {
+        contentBlock.style.cssText = `
+          left: 0;`;
+      }
       this.style.cssText = `
-      transition-duration: 400ms;
-      left: -19.5vw;
-      background: var(--color-theme);`;
+        transition-duration: 400ms;
+        left: -19.5vw;
+        background: var(--color-theme);`;
 
       if(window.MOBILE.isMobile()) {
         this.style.cssText = `
         left: -79vw;`;
       }
     });
+  }
+
+  checkContentBlock() {
+    return document.querySelector('content-x');
   }
 }
 
