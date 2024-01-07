79c885c7baded89365105f48d86d856d33d13955
webnetkz <dbqqbq@gmail.com>
2024-01-07 13:30:51 +0600

    Публикация 0.8.83

--- xpwa.js
+++ xpwa.js
@@ -45,6 +45,6 @@ window.onload = (() => {
     'border-radius: 17px;',
     'color: white;'].join('');
   
-  console.log('%c%s', style, 'xPWA', 'v0.8.83');
+  console.log('%c%s', style, 'xPWA', 'v0.8.83-1');
 })();
 
